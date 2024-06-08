4c7bb84268afd9d1d96bfd39b6d025e9b04b5f0f
Claptrap-maker <lelik575435@gmail.com>
2024-06-08 17:48:56 +0300

    Рефакторинг рассчета маршрутов курьеров

--- src/main/java/ru/ivanova/diplom/logistics/service/OptimizationService.java
+++ src/main/java/ru/ivanova/diplom/logistics/service/OptimizationService.java
@@ -1,5 +1,6 @@
 package ru.ivanova.diplom.logistics.service;
 
+import ch.qos.logback.core.util.StringCollectionUtil;
 import org.apache.commons.math3.ml.clustering.CentroidCluster;
 import org.apache.commons.math3.ml.clustering.Cluster;
 import org.apache.commons.math3.ml.clustering.DoublePoint;
@@ -42,28 +43,28 @@ public class OptimizationService {
             //Удаляем начальную точку (т.к. это точка склада, а не пункта выдачи)
             points.remove(0);
 
-            // Логирование точек
-            System.out.println("Points:");
-            for (DoublePoint point : points) {
-                System.out.println("Point: " + point);
-            }
+//            // Логирование точек
+//            System.out.println("Points:");
+//            for (DoublePoint point : points) {
+//                System.out.println("Point: " + point);
+//            }
 
             // Кластерный анализ
             int minClusters = 2;
             KMeansPlusPlusClusterer<DoublePoint> clusterer = new KMeansPlusPlusClusterer<>(Math.max(minClusters, points.size() / 2), 1000, new EuclideanDistance());
             List<CentroidCluster<DoublePoint>> clusters = clusterer.cluster(points);
 
-            // Логирование кластеров
-            System.out.println("Number of clusters: " + clusters.size());
-            for (Cluster<DoublePoint> cluster : clusters) {
-                System.out.println("Cluster size: " + cluster.getPoints().size());
-                for (DoublePoint point : cluster.getPoints()) {
-                    System.out.println("Point: " + point);
-                }
-            }
+//            // Логирование кластеров
+//            System.out.println("Number of clusters: " + clusters.size());
+//            for (Cluster<DoublePoint> cluster : clusters) {
+//                System.out.println("Cluster size: " + cluster.getPoints().size());
+//                for (DoublePoint point : cluster.getPoints()) {
+//                    System.out.println("Point: " + point);
+//                }
+//            }
 
-            //Логирование startpoint
-            System.out.println("StartPoint: " + startPoint);
+//            //Логирование startpoint
+//            System.out.println("StartPoint: " + startPoint);
 
             // Вычисление центров кластеров
             List<DoublePoint> clusterCenters = new ArrayList<>();
@@ -72,16 +73,16 @@ public class OptimizationService {
                 clusterCenters.add(new DoublePoint(centerPoint));
             }
 
-            // Логирование центров кластеров
-            System.out.println("Centres of clusters: " + clusterCenters.size());
-            clusterCenters.forEach(System.out::println);
+//            // Логирование центров кластеров
+//            System.out.println("Centres of clusters: " + clusterCenters.size());
+//            clusterCenters.forEach(System.out::println);
 
             // Вычисление маршрута для мобильного склада через центры кластеров
             List<DoublePoint> optimizedRoute = calculateRoute(startPoint, clusterCenters);
 
-            // Логирование маршрута
-            System.out.println("Route: " + optimizedRoute.size());
-            optimizedRoute.forEach(System.out::println);
+//            // Логирование маршрута
+//            System.out.println("Route: " + optimizedRoute.size());
+//            optimizedRoute.forEach(System.out::println);
 
             JSONArray optimizedFeatures = new JSONArray();
 
@@ -99,19 +100,30 @@ public class OptimizationService {
             JSONObject lineFeature = createLineFeature(lineCoordinates, "route", "red");
             optimizedFeatures.put(lineFeature);
 
+            // Приоритетная очередь для отслеживания текущей нагрузки каждого курьера
+            PriorityQueue<Courier> pq = new PriorityQueue<>(Comparator.comparingDouble(c -> c.currentDistance));
+
+            // Инициализация очереди курьерами
+            for (int i = 0; i < couriers; i++) {
+                pq.add(new Courier(i, 0, new ArrayList<>()));
+            }
+
             for (CentroidCluster<DoublePoint> cluster : clusters) {
                 List<DoublePoint> clusterPoints = cluster.getPoints();
                 double[] clusterCenter = cluster.getCenter().getPoint();
 
                 List<List<DoublePoint>> courierRoutes = calculateCourierRoutes(new DoublePoint(clusterCenter),
-                        clusterPoints, couriers);
+                        clusterPoints, couriers, pq);
 
                 if (courierRoutes.isEmpty())
                     continue;
 
                 // Логирование маршрута курьера
+                System.out.println("==================================================");
+                System.out.println("clusterCenter: " + new DoublePoint(clusterCenter));
                 System.out.println("courierRoutes: " + courierRoutes.size());
                 courierRoutes.forEach(route -> route.forEach(System.out::println));
+                System.out.println("==================================================");
 
                 for (List<DoublePoint> courierRoute : courierRoutes) {
                     JSONArray courierLineCoordinates = new JSONArray();
@@ -192,7 +204,7 @@ public class OptimizationService {
     }
 
     private List<List<DoublePoint>> calculateCourierRoutes(DoublePoint clusterCenter, List<DoublePoint> clusterPoints,
-                                                           int couriers) {
+                                                           int couriers, PriorityQueue<Courier> pq) {
         List<List<DoublePoint>> courierRoutes = new ArrayList<>();
 
         if (clusterPoints.size() != 1) {
@@ -202,47 +214,94 @@ public class OptimizationService {
                 courierRoutes.get(i).add(clusterCenter); // Начало маршрута с центра кластера
             }
 
-            // Приоритетная очередь для отслеживания текущей нагрузки каждого курьера
-            PriorityQueue<Courier> pq = new PriorityQueue<>(Comparator.comparingDouble(c -> c.currentDistance));
-
-            // Инициализация очереди курьерами
-            for (int i = 0; i < couriers; i++) {
-                pq.add(new Courier(i, 0));
-            }
-
             // Распределение точек по курьерам
             for (DoublePoint point : clusterPoints) {
+                if (point.equals(clusterCenter))
+                    continue;
                 Courier courier = pq.poll();
                 List<DoublePoint> points = courierRoutes.get(courier.id);
-                if (points.size() % 2 == 0) {
-                    points.add(clusterCenter);
+//                // Логгирование добавление точки
+//                System.out.println("===========================================");
+//                System.out.println("Cluster Center: " + clusterCenter);
+//                System.out.println("Point: " + point);
+//                pq.forEach(System.out::println);
+//                System.out.println("===========================================");
+                if (!points.get(points.size()-1).equals(clusterCenter)) {
+                    addingPointAndDistance(clusterCenter, courier, points);
+                    addingPointAndDistance(clusterCenter, courier, points);
+                } else {
+                    courier.visitedPoints.add(point);
+                    courier.countPoints++;
                 }
-                points.add(point);
-                double distanceToAdd = calculateDistance(points
-                        .get(points.size() - 2), point);
-                courier.currentDistance += distanceToAdd;
+                addingPointAndDistance(point, courier, points);
                 pq.add(courier);
             }
 
-            // Завершение маршрута каждого курьера центром кластера
             for (int i = 0; i < couriers; i++) {
-                if (courierRoutes.get(i).size() % 2 == 0) {
-                    courierRoutes.get(i).add(clusterCenter); // Завершение маршрута центром кластера
+                List<DoublePoint> points = courierRoutes.get(i);
+                if (!points.get(points.size() - 1).equals(clusterCenter)) {
+                    for (Courier courier : pq) {
+                        if (courier.id == i) {
+                            addingPointAndDistance(clusterCenter, courier, points);
+                            DoublePoint point = points.get(points.size() - 2);
+                            if (!courier.visitedPoints.contains(point)) {
+                                courier.visitedPoints.add(point);
+                                courier.countPoints++;
+                            }
+                            break;
+                        }
+                    }
+                }
+                else {
+                    points.remove(points.size()-1);
                 }
             }
+
+            // Логгирование курьеров
+            System.out.println("===========================================");
+            System.out.println("Cluster Center: " + clusterCenter);
+            System.out.println("Couriers: " + pq.size());
+            pq.forEach(System.out::println);
+            System.out.println("===========================================");
         }
 
         return courierRoutes;
+    }
+
+    private void addingPointAndDistance(DoublePoint point, Courier courier, List<DoublePoint> points) {
+//        // Логгирование добавления точки
+//        System.out.println("===========================================");
+//        System.out.println("Points size: " + points.size());
+//        System.out.println("Last point in points: " + points.get(points.size()-1));
+//        System.out.println("Point I'm going to add: " + point);
+//        System.out.println("===========================================");
+        points.add(point);
+        double distanceToAdd = calculateDistance(points.get(points.size() - 2), point);
+        courier.currentDistance += distanceToAdd;
     }
 
     // Класс для хранения информации о курьере
     private static class Courier {
         int id;
         double currentDistance;
+        int countPoints;
+        List<DoublePoint> visitedPoints;
 
-        public Courier(int id, double currentDistance) {
+        public Courier(int id, double currentDistance, List<DoublePoint> visitedPoints) {
             this.id = id;
             this.currentDistance = currentDistance;
+            this.visitedPoints = visitedPoints;
+            this.countPoints = visitedPoints.size();
+        }
+
+        @Override
+        public String toString() {
+            return "Courier{" +
+                    "id=" + id +
+                    ", currentDistance=" + currentDistance +
+                    ", countPoints=" + countPoints +
+                    ", visitedPoints=" + visitedPoints.stream().map(doublePoint -> doublePoint.toString() + " ").reduce("", String::concat) +
+                    '}';
         }
     }
 }
